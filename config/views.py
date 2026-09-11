8d4597825d8b7a7769e3b8cfe70c305c629aed4b
Boaz Liebes <boazliebes@MacBook-Pro-sl-bz.local>
2026-09-11 18:37:41 +0300

    Add SeaSlugs video gallery with regional filters and search

---
+++ config/views.py
@@ -0,0 +1,15 @@
+"""Serve the same curated gallery locally and on the static host."""
+from django.conf import settings
+from django.http import FileResponse, Http404
+
+FILES = {
+    'index.html': 'text/html; charset=utf-8',
+    'styles.css': 'text/css; charset=utf-8',
+    'app.js': 'text/javascript; charset=utf-8',
+    'catalog.js': 'text/javascript; charset=utf-8',
+}
+
+def gallery_file(request, filename='index.html'):
+    if filename not in FILES:
+        raise Http404
+    return FileResponse((settings.BASE_DIR / 'dist' / filename).open('rb'), content_type=FILES[filename])
---
+++ dist/catalog.js
@@ -0,0 +1,651 @@
+window.SEASLUGS = {
+  "regions": {
+    "romblon": "רומבלון · פיליפינים",
+    "redsea": "ים סוף · אילת",
+    "mediterranean": "הים התיכון · אכזיב"
+  },
+  "videos": [
+    {
+      "id": "E7TSjlw4aZg",
+      "title": "Aegires sp  7",
+      "region": "romblon",
+      "thumbnail": "https://i.ytimg.com/vi/E7TSjlw4aZg/maxresdefault.jpg"
+    },
+    {
+      "id": "6B-GQ3IFrjs",
+      "title": "Aegires sp  8",
+      "region": "romblon",
+      "thumbnail": "https://i.ytimg.com/vi/6B-GQ3IFrjs/maxresdefault.jpg"
+    },
+    {
+      "id": "5lMcFmUDvyM",
+      "title": "Aegires villosus",
+      "region": "romblon",
+      "thumbnail": "https://i.ytimg.com/vi/5lMcFmUDvyM/maxresdefault.jpg"
+    },
+    {
+      "id": "e-Loi3HKRoE",
+      "title": "Aplysia parvula",
+      "region": "romblon",
+      "thumbnail": "https://i.ytimg.com/vi/e-Loi3HKRoE/maxresdefault.jpg"
+    },
+    {
+      "id": "L7DbhTX1VAA",
+      "title": "Asteronotus cespitosus",
+      "region": "romblon",
+      "thumbnail": "https://i.ytimg.com/vi/L7DbhTX1VAA/maxresdefault.jpg"
+    },
+    {
+      "id": "sTmjqSZ_x0I",
+      "title": "Atagema sp  12",
+      "region": "romblon",
+      "thumbnail": "https://i.ytimg.com/vi/sTmjqSZ_x0I/maxresdefault.jpg"
+    },
+    {
+      "id": "oQWJAINRLWA",
+      "title": "Atagema sp 13",
+      "region": "romblon",
+      "thumbnail": "https://i.ytimg.com/vi/oQWJAINRLWA/maxresdefault.jpg"
+    },
+    {
+      "id": "CT1-bImgR5g",
+      "title": "Bermudella brunneomaculata",
+      "region": "romblon",
+      "thumbnail": "https://i.ytimg.com/vi/CT1-bImgR5g/maxresdefault.jpg"
+    },
+    {
+      "id": "ySurLh3kMuA",
+      "title": "Berthellina delicata",
+      "region": "romblon",
+      "thumbnail": "https://i.ytimg.com/vi/ySurLh3kMuA/maxresdefault.jpg"
+    },
+    {
+      "id": "55_cNnHQiHw",
+      "title": "Biuve fulvipunctata",
+      "region": "romblon",
+      "thumbnail": "https://i.ytimg.com/vi/55_cNnHQiHw/maxresdefault.jpg"
+    },
+    {
+      "id": "srmvchqYiR8",
+      "title": "Bornella anguilla",
+      "region": "romblon",
+      "thumbnail": "https://i.ytimg.com/vi/srmvchqYiR8/maxresdefault.jpg"
+    },
+    {
+      "id": "gc8MVSTLxzU",
+      "title": "Caloria indica",
+      "region": "romblon",
+      "thumbnail": "https://i.ytimg.com/vi/gc8MVSTLxzU/maxresdefault.jpg"
+    },
+    {
+      "id": "1uBbftibmv0",
+      "title": "Chelidonura amoena",
+      "region": "romblon",
+      "thumbnail": "https://i.ytimg.com/vi/1uBbftibmv0/maxresdefault.jpg"
+    },
+    {
+      "id": "zwmooFtDABY",
+      "title": "Chelidonura varians",
+      "region": "romblon",
+      "thumbnail": "https://i.ytimg.com/vi/zwmooFtDABY/maxresdefault.jpg"
+    },
+    {
+      "id": "JSOFLtDHZuA",
+      "title": "Chromodoris annae",
+      "region": "romblon",
+      "thumbnail": "https://i.ytimg.com/vi/JSOFLtDHZuA/maxresdefault.jpg"
+    },
+    {
+      "id": "ZmsZlUH5q3o",
+      "title": "Chromodoris cf  strigata",
+      "region": "romblon",
+      "thumbnail": "https://i.ytimg.com/vi/ZmsZlUH5q3o/maxresdefault.jpg"
+    },
+    {
+      "id": "5Onsm3_FJTI",
+      "title": "Chromodoris kalawakan",
+      "region": "romblon",
+      "thumbnail": "https://i.ytimg.com/vi/5Onsm3_FJTI/maxresdefault.jpg"
+    },
+    {
+      "id": "vFQxpmWE0w0",
+      "title": "Chromodoris lochi",
+      "region": "romblon",
+      "thumbnail": "https://i.ytimg.com/vi/vFQxpmWE0w0/maxresdefault.jpg"
+    },
+    {
+      "id": "4fspt5sEs_k",
+      "title": "Chromodoris magnifica",
+      "region": "romblon",
+      "thumbnail": "https://i.ytimg.com/vi/4fspt5sEs_k/maxresdefault.jpg"
+    },
+    {
+      "id": "K71kujH4igY",
+      "title": "Chromodoris quadricolor",
+      "region": "redsea",
+      "thumbnail": "https://i.ytimg.com/vi/K71kujH4igY/maxresdefault.jpg"
+    },
+    {
+      "id": "3gcQaiHLRn4",
+      "title": "Chromodoris willani",
+      "region": "romblon",
+      "thumbnail": "https://i.ytimg.com/vi/3gcQaiHLRn4/maxresdefault.jpg"
+    },
+    {
+      "id": "G7SZC2n0B24",
+      "title": "Colpodaspis thompsoni",
+      "region": "romblon",
+      "thumbnail": "https://i.ytimg.com/vi/G7SZC2n0B24/maxresdefault.jpg"
+    },
+    {
+      "id": "oNruKrMZ_eM",
+      "title": "Coryphellina delicata",
+      "region": "romblon",
+      "thumbnail": "https://i.ytimg.com/vi/oNruKrMZ_eM/maxresdefault.jpg"
+    },
+    {
+      "id": "LWA-IDwMyqU",
+      "title": "Coryphellina exoptata",
+      "region": "romblon",
+      "thumbnail": "https://i.ytimg.com/vi/LWA-IDwMyqU/maxresdefault.jpg"
+    },
+    {
+      "id": "fQP3TzXkuvc",
+      "title": "Coryphellina flamma",
+      "region": "romblon",
+      "thumbnail": "https://i.ytimg.com/vi/fQP3TzXkuvc/maxresdefault.jpg"
+    },
+    {
+      "id": "vBx39enpB8Q",
+      "title": "Coryphellina iurmanovi",
+      "region": "romblon",
+      "thumbnail": "https://i.ytimg.com/vi/vBx39enpB8Q/maxresdefault.jpg"
+    },
+    {
+      "id": "WSZAiNg4Azw",
+      "title": "Coryphellina sp  A",
+      "region": "romblon",
+      "thumbnail": "https://i.ytimg.com/vi/WSZAiNg4Azw/maxresdefault.jpg"
+    },
+    {
+      "id": "fnEsY87GPM0",
+      "title": "Coryphellina sp  B",
+      "region": "romblon",
+      "thumbnail": "https://i.ytimg.com/vi/fnEsY87GPM0/maxresdefault.jpg"
+    },
+    {
+      "id": "e3biVGIOvOs",
+      "title": "Costasiella kuroshimae",
+      "region": "romblon",
+      "thumbnail": "https://i.ytimg.com/vi/e3biVGIOvOs/maxresdefault.jpg"
+    },
+    {
+      "id": "FSBiVh07KWg",
+      "title": "Costasiella usagi",
+      "region": "romblon",
+      "thumbnail": "https://i.ytimg.com/vi/FSBiVh07KWg/maxresdefault.jpg"
+    },
+    {
+      "id": "PT70K7huIeg",
+      "title": "Cratena simba",
+      "region": "romblon",
+      "thumbnail": "https://i.ytimg.com/vi/PT70K7huIeg/maxresdefault.jpg"
+    },
+    {
+      "id": "bE5wwpX76vY",
+      "title": "Cratena sp",
+      "region": "romblon",
+      "thumbnail": "https://i.ytimg.com/vi/bE5wwpX76vY/maxresdefault.jpg"
+    },
+    {
+      "id": "yF5vTWBrvJA",
+      "title": "Cyerce basi",
+      "region": "romblon",
+      "thumbnail": "https://i.ytimg.com/vi/yF5vTWBrvJA/maxresdefault.jpg"
+    },
+    {
+      "id": "Ic9GPZbniPc",
+      "title": "Cyerce nigra",
+      "region": "romblon",
+      "thumbnail": "https://i.ytimg.com/vi/Ic9GPZbniPc/maxresdefault.jpg"
+    },
+    {
+      "id": "820JZUu0FKw",
+      "title": "Dendrodoris tuberculosa",
+      "region": "romblon",
+      "thumbnail": "https://i.ytimg.com/vi/820JZUu0FKw/maxresdefault.jpg"
+    },
+    {
+      "id": "SG2jtcQ8fqc",
+      "title": "Dermatobranchus sp  17",
+      "region": "romblon",
+      "thumbnail": "https://i.ytimg.com/vi/SG2jtcQ8fqc/maxresdefault.jpg"
+    },
+    {
+      "id": "a8U_ZjQ90C4",
+      "title": "Diniatys dentifer",
+      "region": "romblon",
+      "thumbnail": "https://i.ytimg.com/vi/a8U_ZjQ90C4/maxresdefault.jpg"
+    },
+    {
+      "id": "ZSHTkDcOsOM",
+      "title": "Discodoris cebuensis",
+      "region": "romblon",
+      "thumbnail": "https://i.ytimg.com/vi/ZSHTkDcOsOM/maxresdefault.jpg"
+    },
+    {
+      "id": "gDHJwbZnxqc",
+      "title": "Diversidoris crocea",
+      "region": "romblon",
+      "thumbnail": "https://i.ytimg.com/vi/gDHJwbZnxqc/maxresdefault.jpg"
+    },
+    {
+      "id": "5Pg5ohodApk",
+      "title": "Doridomorpha gardineri",
+      "region": "romblon",
+      "thumbnail": "https://i.ytimg.com/vi/5Pg5ohodApk/maxresdefault.jpg"
+    },
+    {
+      "id": "DB8-IZYjLqE",
+      "title": "Doriprismatica balut",
+      "region": "romblon",
+      "thumbnail": "https://i.ytimg.com/vi/DB8-IZYjLqE/maxresdefault.jpg"
+    },
+    {
+      "id": "aX0_N8kNnvs",
+      "title": "Doto sp",
+      "region": "romblon",
+      "thumbnail": "https://i.ytimg.com/vi/aX0_N8kNnvs/maxresdefault.jpg"
+    },
+    {
+      "id": "kxd1SjVxNUc",
+      "title": "Elysia cf  pusilla",
+      "region": "romblon",
+      "thumbnail": "https://i.ytimg.com/vi/kxd1SjVxNUc/maxresdefault.jpg"
+    },
+    {
+      "id": "Mtn1g-eUZf4",
+      "title": "Elysia nigropunctata",
+      "region": "romblon",
+      "thumbnail": "https://i.ytimg.com/vi/Mtn1g-eUZf4/maxresdefault.jpg"
+    },
+    {
+      "id": "cEs4eAzqTD4",
+      "title": "Elysia sp  A",
+      "region": "romblon",
+      "thumbnail": "https://i.ytimg.com/vi/cEs4eAzqTD4/maxresdefault.jpg"
+    },
+    {
+      "id": "a__B2D4yaEY",
+      "title": "Elysia sp  B",
+      "region": "romblon",
+      "thumbnail": "https://i.ytimg.com/vi/a__B2D4yaEY/maxresdefault.jpg"
+    },
+    {
+      "id": "wk3uQu5qurI",
+      "title": "Ercolania sp",
+      "region": "romblon",
+      "thumbnail": "https://i.ytimg.com/vi/wk3uQu5qurI/maxresdefault.jpg"
+    },
+    {
+      "id": "8WjuIniYp7Y",
+      "title": "Eubranchus sp  15",
+      "region": "romblon",
+      "thumbnail": "https://i.ytimg.com/vi/8WjuIniYp7Y/maxresdefault.jpg"
+    },
+    {
+      "id": "eSIUTQHp6rs",
+      "title": "Eubranchus sp  17",
+      "region": "romblon",
+      "thumbnail": "https://i.ytimg.com/vi/eSIUTQHp6rs/maxresdefault.jpg"
+    },
+    {
+      "id": "T_10kmLWQYo",
+      "title": "Eubranchus sp  2",
+      "region": "romblon",
+      "thumbnail": "https://i.ytimg.com/vi/T_10kmLWQYo/maxresdefault.jpg"
+    },
+    {
+      "id": "Msi1TxUq9yQ",
+      "title": "Eubranchus sp  23",
+      "region": "romblon",
+      "thumbnail": "https://i.ytimg.com/vi/Msi1TxUq9yQ/maxresdefault.jpg"
+    },
+    {
+      "id": "-D5YoEdu6nA",
+      "title": "Eubranchus sp. 26",
+      "region": "romblon",
+      "thumbnail": "https://i.ytimg.com/vi/-D5YoEdu6nA/maxresdefault.jpg"
+    },
+    {
+      "id": "0moTXRHebTI",
+      "title": "Eubranchus sp. 27",
+      "region": "romblon",
+      "thumbnail": "https://i.ytimg.com/vi/0moTXRHebTI/maxresdefault.jpg"
+    },
+    {
+      "id": "Ps6hvPCHGPg",
+      "title": "Facelina sp. 4",
+      "region": "romblon",
+      "thumbnail": "https://i.ytimg.com/vi/Ps6hvPCHGPg/maxresdefault.jpg"
+    },
+    {
+      "id": "N5CjMFJXoi8",
+      "title": "Favorinus mirabilis",
+      "region": "romblon",
+      "thumbnail": "https://i.ytimg.com/vi/N5CjMFJXoi8/maxresdefault.jpg"
+    },
+    {
+      "id": "EAbyTdludFg",
+      "title": "Favorinus sp. 10",
+      "region": "romblon",
+      "thumbnail": "https://i.ytimg.com/vi/EAbyTdludFg/maxresdefault.jpg"
+    },
+    {
+      "id": "C37TZiueLBU",
+      "title": "Favorinus tsuruganus",
+      "region": "romblon",
+      "thumbnail": "https://i.ytimg.com/vi/C37TZiueLBU/maxresdefault.jpg"
+    },
+    {
+      "id": "2AlUwTtyE1A",
+      "title": "Glossodoris cf. cincta",
+      "region": "romblon",
+      "thumbnail": "https://i.ytimg.com/vi/2AlUwTtyE1A/maxresdefault.jpg"
+    },
+    {
+      "id": "AH5LipsEmog",
+      "title": "Glossodoris rufomarginata",
+      "region": "romblon",
+      "thumbnail": "https://i.ytimg.com/vi/AH5LipsEmog/maxresdefault.jpg"
+    },
+    {
+      "id": "VzfQF9U2w4I",
+      "title": "Goniobranchus fidelis",
+      "region": "romblon",
+      "thumbnail": "https://i.ytimg.com/vi/VzfQF9U2w4I/maxresdefault.jpg"
+    },
+    {
+      "id": "pUIepJE2YMo",
+      "title": "Goniobranchus geometricus",
+      "region": "romblon",
+      "thumbnail": "https://i.ytimg.com/vi/pUIepJE2YMo/maxresdefault.jpg"
+    },
+    {
+      "id": "AQSv5K6oWMs",
+      "title": "Goniobranchus hintuanensis",
+      "region": "romblon",
+      "thumbnail": "https://i.ytimg.com/vi/AQSv5K6oWMs/maxresdefault.jpg"
+    },
+    {
+      "id": "lrXWA0P9hP4",
+      "title": "Goniobranchus kuniei",
+      "region": "romblon",
+      "thumbnail": "https://i.ytimg.com/vi/lrXWA0P9hP4/maxresdefault.jpg"
+    },
+    {
+      "id": "Alqr3CYjqhU",
+      "title": "Goniobranchus sp",
+      "region": "romblon",
+      "thumbnail": "https://i.ytimg.com/vi/Alqr3CYjqhU/maxresdefault.jpg"
+    },
+    {
+      "id": "-PCfSei4L2E",
+      "title": "Goniodoridella geminae",
+      "region": "romblon",
+      "thumbnail": "https://i.ytimg.com/vi/-PCfSei4L2E/maxresdefault.jpg"
+    },
+    {
+      "id": "ied6kVQgQh4",
+      "title": "Goniodoridella savignyi",
+      "region": "romblon",
+      "thumbnail": "https://i.ytimg.com/vi/ied6kVQgQh4/maxresdefault.jpg"
+    },
+    {
+      "id": "F_sj_suey0g",
+      "title": "Goniodoridella unidonta",
+      "region": "romblon",
+      "thumbnail": "https://i.ytimg.com/vi/F_sj_suey0g/maxresdefault.jpg"
+    },
+    {
+      "id": "65-AHQ8BewY",
+      "title": "Gymnodoris cf. citrina",
+      "region": "romblon",
+      "thumbnail": "https://i.ytimg.com/vi/65-AHQ8BewY/maxresdefault.jpg"
+    },
+    {
+      "id": "sOeTBq_JkJI",
+      "title": "Gymnodoris tuberculosa",
+      "region": "romblon",
+      "thumbnail": "https://i.ytimg.com/vi/sOeTBq_JkJI/maxresdefault.jpg"
+    },
+    {
+      "id": "xh24fqmjB58",
+      "title": "Halgerda carlsoni",
+      "region": "romblon",
+      "thumbnail": "https://i.ytimg.com/vi/xh24fqmjB58/maxresdefault.jpg"
+    },
+    {
+      "id": "aZ39RYioSQM",
+      "title": "Haminoea sp. 5",
+      "region": "romblon",
+      "thumbnail": "https://i.ytimg.com/vi/aZ39RYioSQM/maxresdefault.jpg"
+    },
+    {
+      "id": "CHkx8kZBDE8",
+      "title": "Haminoeid sp. 2",
+      "region": "romblon",
+      "thumbnail": "https://i.ytimg.com/vi/CHkx8kZBDE8/maxresdefault.jpg"
+    },
+    {
+      "id": "hpKugQX_R4w",
+      "title": "Hexabranchus lacer juv",
+      "region": "romblon",
+      "thumbnail": "https://i.ytimg.com/vi/hpKugQX_R4w/maxresdefault.jpg"
+    },
+    {
+      "id": "Nx1eI5Q27pQ",
+      "title": "Hypselodoris cerisae",
+      "region": "romblon",
+      "thumbnail": "https://i.ytimg.com/vi/Nx1eI5Q27pQ/maxresdefault.jpg"
+    },
+    {
+      "id": "Bux2h4W0s5M",
+      "title": "Hypselodoris decorata",
+      "region": "romblon",
+      "thumbnail": "https://i.ytimg.com/vi/Bux2h4W0s5M/maxresdefault.jpg"
+    },
+    {
+      "id": "PhByOy7iHFA",
+      "title": "Hypselodoris infucata",
+      "region": "romblon",
+      "thumbnail": "https://i.ytimg.com/vi/PhByOy7iHFA/maxresdefault.jpg"
+    },
+    {
+      "id": "wMYmYSo466A",
+      "title": "Hypselodoris reidi",
+      "region": "romblon",
+      "thumbnail": "https://i.ytimg.com/vi/wMYmYSo466A/maxresdefault.jpg"
+    },
+    {
+      "id": "Ix1XHA771TI",
+      "title": "Hypselodoris skyleri",
+      "region": "romblon",
+      "thumbnail": "https://i.ytimg.com/vi/Ix1XHA771TI/maxresdefault.jpg"
+    },
+    {
+      "id": "0ULDohhqIX8",
+      "title": "Hypselodoris tryoni",
+      "region": "romblon",
+      "thumbnail": "https://i.ytimg.com/vi/0ULDohhqIX8/maxresdefault.jpg"
+    },
+    {
+      "id": "gKnPS29sAUk",
+      "title": "Kabeiro phasmida",
+      "region": "romblon",
+      "thumbnail": "https://i.ytimg.com/vi/gKnPS29sAUk/maxresdefault.jpg"
+    },
+    {
+      "id": "FTjX6ylyjSM",
+      "title": "Kaloplocamus acutus",
+      "region": "romblon",
+      "thumbnail": "https://i.ytimg.com/vi/FTjX6ylyjSM/maxresdefault.jpg"
+    },
+    {
+      "id": "7KGcP7IZiOw",
+      "title": "Lamprohaminoea evelinae",
+      "region": "romblon",
+      "thumbnail": "https://i.ytimg.com/vi/7KGcP7IZiOw/maxresdefault.jpg"
+    },
+    {
+      "id": "_JuwAAs1tr0",
+      "title": "Marionia sp  24 juv",
+      "region": "romblon",
+      "thumbnail": "https://i.ytimg.com/vi/_JuwAAs1tr0/maxresdefault.jpg"
+    },
+    {
+      "id": "X9kcR-NRveY",
+      "title": "Marioniopsis arborescens",
+      "region": "romblon",
+      "thumbnail": "https://i.ytimg.com/vi/X9kcR-NRveY/maxresdefault.jpg"
+    },
+    {
+      "id": "4APlv5cShfs",
+      "title": "Melibe colemani",
+      "region": "romblon",
+      "thumbnail": "https://i.ytimg.com/vi/4APlv5cShfs/maxresdefault.jpg"
+    },
+    {
+      "id": "5lVgDpvNV5Y",
+      "title": "Melibe digitata",
+      "region": "romblon",
+      "thumbnail": "https://i.ytimg.com/vi/5lVgDpvNV5Y/maxresdefault.jpg"
+    },
+    {
+      "id": "e95bNAPUo1U",
+      "title": "Melibe engeli",
+      "region": "romblon",
+      "thumbnail": "https://i.ytimg.com/vi/e95bNAPUo1U/maxresdefault.jpg"
+    },
+    {
+      "id": "_TqifxStsME",
+      "title": "Mexichromis aurora",
+      "region": "romblon",
+      "thumbnail": "https://i.ytimg.com/vi/_TqifxStsME/maxresdefault.jpg"
+    },
+    {
+      "id": "qm-ZyEkti8k",
+      "title": "Miamira sinuata",
+      "region": "romblon",
+      "thumbnail": "https://i.ytimg.com/vi/qm-ZyEkti8k/maxresdefault.jpg"
+    },
+    {
+      "id": "DJeF9h1OtFM",
+      "title": "Murphydoris maracabranchia",
+      "region": "romblon",
+      "thumbnail": "https://i.ytimg.com/vi/DJeF9h1OtFM/maxresdefault.jpg"
+    },
+    {
+      "id": "xvI3oOGVyig",
+      "title": "Nembrotha cristata",
+      "region": "romblon",
+      "thumbnail": "https://i.ytimg.com/vi/xvI3oOGVyig/maxresdefault.jpg"
+    },
+    {
+      "id": "W-MmamfdYj0",
+      "title": "Nembrotha kubaryana",
+      "region": "romblon",
+      "thumbnail": "https://i.ytimg.com/vi/W-MmamfdYj0/maxresdefault.jpg"
+    },
+    {
+      "id": "BsFTWPkRMUQ",
+      "title": "Nembrotha lineolata",
+      "region": "romblon",
+      "thumbnail": "https://i.ytimg.com/vi/BsFTWPkRMUQ/maxresdefault.jpg"
+    },
+    {
+      "id": "FHDOr1jrpag",
+      "title": "Nembrotha sp",
+      "region": "romblon",
+      "thumbnail": "https://i.ytimg.com/vi/FHDOr1jrpag/maxresdefault.jpg"
+    },
+    {
+      "id": "MPQMEluDGz4",
+      "title": "Nembrotha sp. 5",
+      "region": "romblon",
+      "thumbnail": "https://i.ytimg.com/vi/MPQMEluDGz4/maxresdefault.jpg"
+    },
+    {
+      "id": "loiOXH-DFXI",
+      "title": "Noumeaella sp. 3",
+      "region": "romblon",
+      "thumbnail": "https://i.ytimg.com/vi/loiOXH-DFXI/maxresdefault.jpg"
+    },
+    {
+      "id": "Wm7oxm_-loY",
+      "title": "Phanerophthalmus anettae",
+      "region": "romblon",
+      "thumbnail": "https://i.ytimg.com/vi/Wm7oxm_-loY/maxresdefault.jpg"
+    },
+    {
+      "id": "VAlxlBhzCLg",
+      "title": "Phestilla lugubris",
+      "region": "romblon",
+      "thumbnail": "https://i.ytimg.com/vi/VAlxlBhzCLg/maxresdefault.jpg"
+    },
+    {
+      "id": "7YXbfNroHd8",
+      "title": "Phestilla sibogae",
+      "region": "romblon",
+      "thumbnail": "https://i.ytimg.com/vi/7YXbfNroHd8/maxresdefault.jpg"
+    },
+    {
+      "id": "yZ-l6YM-hKU",
+      "title": "Phyllidia coelestis",
+      "region": "romblon",
+      "thumbnail": "https://i.ytimg.com/vi/yZ-l6YM-hKU/maxresdefault.jpg"
+    },
+    {
+      "id": "_vKoazyr-4s",
+      "title": "Phyllidia elegans",
+      "region": "romblon",
+      "thumbnail": "https://i.ytimg.com/vi/_vKoazyr-4s/maxresdefault.jpg"
+    },
+    {
+      "id": "n1mY2LmfuS8",
+      "title": "Phyllidia ocellata",
+      "region": "romblon",
+      "thumbnail": "https://i.ytimg.com/vi/n1mY2LmfuS8/maxresdefault.jpg"
+    },
+    {
+      "id": "64HzkUeAwLE",
+      "title": "Phyllidia sp. 1",
+      "region": "romblon",
+      "thumbnail": "https://i.ytimg.com/vi/64HzkUeAwLE/maxresdefault.jpg"
+    },
+    {
+      "id": "w3GiiG3EBX8",
+      "title": "Phyllidiella pustulosa RedSea",
+      "region": "redsea",
+      "thumbnail": "https://i.ytimg.com/vi/w3GiiG3EBX8/maxresdefault.jpg"
+    },
+    {
+      "id": "XN9IGI5FAsM",
+      "title": "Romblon 2026 SlideShow",
+      "region": "romblon",
+      "thumbnail": "https://i.ytimg.com/vi/XN9IGI5FAsM/maxresdefault.jpg"
+    },
+    {
+      "id": "4z1ezvGEbFI",
+      "title": "קניון אכזיב יולי 23",
+      "region": "mediterranean",
+      "thumbnail": "https://i.ytimg.com/vi/4z1ezvGEbFI/maxresdefault.jpg"
+    },
+    {
+      "id": "CMQNMndnu3Y",
+      "title": "𝘾𝙮𝙚𝙧𝙘𝙚 𝙗𝙡𝙖𝙘𝙠𝙗𝙪𝙧𝙣𝙖𝙚",
+      "region": "romblon",
+      "thumbnail": "https://i.ytimg.com/vi/CMQNMndnu3Y/maxresdefault.jpg"
+    }
+  ]
+};
